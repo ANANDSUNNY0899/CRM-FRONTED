c8c73d84f7d5867c4eadeac8cfcc511be22279d1
aadarshraj4321 <asunny583@gmail.com>
2025-05-13 18:57:04 +0530

    adding the backend deploy api

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = "http://localhost:5000"; 
+const API = "https://crm-backend-1-9x3q.onrender.com/"; 
 
 export const loginUser = async (email, password) => {
   return axios.post(`${API}/login`, { email, password });
